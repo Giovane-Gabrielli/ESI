1fb7d4e5247e1793d6558e199c72193487f88e0d
marcorush <acmarco1991@gmail.com>
2016-10-18 00:44:40 +0000

    Novo teste da página aprovadas e novos links de navegação

--- features/step_definitions/aprovadas_steps.rb
+++ features/step_definitions/aprovadas_steps.rb
@@ -22,6 +22,10 @@ When(/^I click on (.*)$/) do |btn|
   click_button btn
 end
 
+When(/^I follow Ver todas as matérias de SI$/) do
+  click_link('to-si')
+end
+
 # Then
 Then(/^I should be on the Disciplinas já aprovadas page$/) do
   expect(page).to have_content("Selecione disciplinas já aprovadas")
@@ -33,4 +37,8 @@ end
 
 Then(/^(.*) must not be checked$/) do |dis|
   page.has_no_checked_field?(dis)
+end
+
+Then(/^I must be redirect to Disciplinas de SI$/) do
+  expect(page).to have_content("Disciplinas do curso de SI")
 end
